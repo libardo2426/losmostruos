5996fc8d08d610df78d06802677f16dbb05cd73c
libardo <libardo2426@gmail.com>
2018-08-07 20:42:29 +0000

    cambio de base de datos a postgres

--- monstruos/settings.py
+++ monstruos/settings.py
@@ -118,17 +118,12 @@ WSGI_APPLICATION = 'monstruos.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'c9',
-        'USER': 'libardo',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'losmostruos',
+        'USER': 'losmostruos',
+        'PASSWORD': 'delunoal8',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
